213765a6ef4767e6276b5c4081c0f2b4a61e7ca2
LeeThiep <lethithiep131002@gmail.com>
2024-02-28 09:35:52 +0700

    [update] UI

--- Spellcheck/javascript.js
+++ Spellcheck/javascript.js
@@ -1,4 +1,3 @@
-
 var wordSuggestions = {}; //  lưu trữ danh sách gợi ý
 
 document.addEventListener("DOMContentLoaded", function () {
@@ -27,77 +26,78 @@ function getSuggestedWords(word) {
   return dictionary[word.toLowerCase()] || [];
 }
 
-
 function showPopup(element) {
-    const popupElement = document.querySelector('.popup');
-    const rect = element.getBoundingClientRect();
-    const top = rect.top + window.pageYOffset;
-    const left = rect.left + window.pageXOffset;
-  
-    popupElement.style.top = `${top + element.offsetHeight}px`;
-    popupElement.style.left = `${left}px`;
-  
-    currentIndex = 0; // Reset currentIndex khi mở popup
-    var word = element.innerText.toLowerCase();
-    var suggestedWords = wordSuggestions[word]; // Lấy danh sách từ gợi ý cho từ hiện tại
-    var popup = document.getElementById("popup");
-    popup.innerHTML =
-      "<span class='close-btn' onclick='closePopup()'>X</span><b>Did you mean:</b>";
-    var ul = document.createElement("ul");
-    var li = document.createElement("li");
-  
-    li.textContent = suggestedWords[currentIndex];
-    li.setAttribute(
-      "onclick",
-      "replaceWord(this, '" + element.innerText.toLowerCase() + "')"
-    );
-    popup.innerHTML +=
-      "<p class='wrongWord'> " + element.innerText + "</p>"; 
-    ul.appendChild(li);
-    popup.appendChild(ul);
-  
-    // Hiển thị currentIndex
-    popup.innerHTML += "<span class='currentIndex'>" + (currentIndex + 1) + "/" + suggestedWords.length + "</span>";
-  
-    popup.innerHTML +=
-      "<span class='navigation' onclick='prevWord()'> &lt; </span> <span class='navigation' onclick='nextWord()'> &gt; </span>";
-    popup.innerHTML +=
-      "<button class ='ignoreBtn' onclick='ignoreBtn(this, \"" +
-      element.innerText.toLowerCase() +
-      "\")'>ignore</button>";
-    popup.innerHTML +=
-      "<button class ='confirmBtn' onclick='confirmBtn(this, \"" +
-      element.innerText.toLowerCase() +
-      "\")'>Confirm</button>";
-    popup.style.display = "block";
-    popup.suggestedWords = suggestedWords;
-  }
-  
-  function prevWord() {
-    var popup = document.getElementById("popup");
-    var suggestedWords = popup.suggestedWords;
-    currentIndex =
-      (currentIndex - 1 + suggestedWords.length) % suggestedWords.length;
-    popup
-      .getElementsByTagName("ul")[0]
-      .getElementsByTagName("li")[0].textContent = suggestedWords[currentIndex];
-  
-    // Cập nhật currentIndex trong popup
-    popup.querySelector('.currentIndex').textContent = (currentIndex + 1) + "/" + suggestedWords.length;
-  }
-  
-  function nextWord() {
-    var popup = document.getElementById("popup");
-    var suggestedWords = popup.suggestedWords;
-    currentIndex = (currentIndex + 1) % suggestedWords.length;
-    popup
-      .getElementsByTagName("ul")[0]
-      .getElementsByTagName("li")[0].textContent = suggestedWords[currentIndex];
-  
-    // Cập nhật currentIndex trong popup
-    popup.querySelector('.currentIndex').textContent = (currentIndex + 1) + "/" + suggestedWords.length;
-  }
-  
+  const popupElement = document.querySelector(".popup");
+  const rect = element.getBoundingClientRect();
+  const top = rect.top + window.pageYOffset;
+  const left = rect.left + window.pageXOffset;
+
+  popupElement.style.top = `${top + element.offsetHeight}px`;
+  popupElement.style.left = `${left}px`;
+
+  currentIndex = 0; // Reset currentIndex khi mở popup
+  var word = element.innerText.toLowerCase();
+  var suggestedWords = wordSuggestions[word]; // Lấy danh sách từ gợi ý cho từ hiện tại
+  var popup = document.getElementById("popup");
+  popup.innerHTML = ""; // Clear previous content
+
+  popup.innerHTML += `
+    
+    <div class="popup-content">
+    <span class='close-btn' onclick='closePopup()'><i class='fas fa-times'></i></span>
+      <b>Did you mean:</b>
+      <ul>
+        <li class="suggested-words" onclick='replaceWord(this, "${element.innerText.toLowerCase()}")'>${
+    suggestedWords[currentIndex]
+  }</li>
+      </ul>
+      <div class='popup-control'>
+      <div class='popup--control__left'>
+      <span class='currentIndex'>${currentIndex + 1}/${
+    suggestedWords.length
+  }</span>
+      <span class='navigation' onclick='prevWord()'><i class='fas fa-chevron-left'></i></span>
+      <span class='navigation' onclick='nextWord()'><i class='fas fa-chevron-right'></i></span>
+      </div>
+      <div class='popup--control__right'>
+      <button class ='ignoreBtn' onclick='ignoreBtn(this, "${element.innerText.toLowerCase()}")'>Ignore</button>
+      <button class ='confirmBtn' onclick='confirmBtn(this, "${element.innerText.toLowerCase()}")'>Confirm</button>
+      </div>
+      </div>
+      
+    </div>`;
+
+  popup.style.display = "block";
+  popup.suggestedWords = suggestedWords;
+}
+
+function prevWord() {
+  var popup = document.getElementById("popup");
+  var suggestedWords = popup.suggestedWords;
+  currentIndex =
+    (currentIndex - 1 + suggestedWords.length) % suggestedWords.length;
+  popup
+    .getElementsByTagName("ul")[0]
+    .getElementsByTagName("li")[0].textContent = suggestedWords[currentIndex];
+
+  // Cập nhật currentIndex trong popup
+  popup.querySelector(".currentIndex").textContent =
+    currentIndex + 1 + "/" + suggestedWords.length;
+}
+
+function nextWord() {
+  var popup = document.getElementById("popup");
+  var suggestedWords = popup.suggestedWords;
+  currentIndex = (currentIndex + 1) % suggestedWords.length;
+  popup
+    .getElementsByTagName("ul")[0]
+    .getElementsByTagName("li")[0].textContent = suggestedWords[currentIndex];
+
+  // Cập nhật currentIndex trong popup
+  popup.querySelector(".currentIndex").textContent =
+    currentIndex + 1 + "/" + suggestedWords.length;
+}
+
 function closePopup() {
   var popup = document.getElementById("popup");
   popup.style.display = "none";
